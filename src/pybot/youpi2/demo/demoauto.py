df65fcdcc953e7f208552a57464a1bc4748350de
Eric Pascual <eric@pobot.org>
2016-08-21 18:49:33 +0200

    added SIGTERM handling

--- src/pybot/youpi2/demo/demoauto.py
+++ src/pybot/youpi2/demo/demoauto.py
@@ -3,6 +3,7 @@
 import argparse
 import time
 import datetime
+import signal
 
 from pybot.youpi2.ctlpanel import ControlPanel
 from pybot.youpi2.ctlpanel.devices.fs import FileSystemDevice
@@ -22,12 +23,17 @@ class DemoAuto(object):
 
     def __init__(self, pnldev="/mnt/lcdfs"):
         self.pnl = ControlPanel(FileSystemDevice(pnldev))
+        self.terminated = False
+
+    def terminate(self):
+        self.terminated = True
 
     def run(self):
+        signal.signal(signal.SIGTERM, self.terminate)
         self.pnl.clear()
         self.pnl.center_text_at("Automatic demo", line=1)
 
-        while True:
+        while not self.terminated:
             self.pnl.center_text_at(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), line=3)
 
             time.sleep(1)
