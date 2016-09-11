de8455c2242de98dae60bcf5b505d8d8085584d8
Eric Pascual <eric@pobot.org>
2016-09-11 22:29:08 +0200

    sequence format reworked

--- src/pybot/youpi2/demo/app.py
+++ src/pybot/youpi2/demo/app.py
@@ -2,6 +2,7 @@
 
 import pkgutil
 import json
+import time
 
 from pybot.youpi2.app import YoupiApplication
 from pybot.youpi2.model import YoupiArm
@@ -16,36 +17,117 @@ class AutomaticDemoApp(YoupiApplication):
     TITLE = "Standalone demo"
     VERSION = version
 
-    sequence = None
+    STATE_IDLE, STATE_LOADED, STATE_EXECUTING = range(3)
+
+    sequence = []
     steps_count = 0
     step_num = 0
-    steps_done = -1
+    current_step = None
+    state = STATE_IDLE
 
     def add_custom_arguments(self, parser):
         parser.add_argument('--sequence-name', default="test")
 
     def setup(self, sequence_name="test", **kwargs):
-        self.sequence = json.loads(pkgutil.get_data('pybot.youpi2.demo', 'data/%s.json' % sequence_name))
+        src = json.loads(pkgutil.get_data('pybot.youpi2.demo', 'data/%s.json' % sequence_name))
+        for keyw, args in src:
+            try:
+                step_class = _STATEMENTS[keyw]
+            except KeyError:
+                raise ValueError('invalid keyword: %s' % keyw)
+            else:
+                self.sequence.append(step_class(**args))
+
         self.steps_count = len(self.sequence)
         self.logger.info("sequence: %s", self.sequence)
 
     def loop(self):
-        # if the arm is not moving, advance to the next sequence step,
-        # looping when reaching its end (apart if single step sequence)
-        if not self.arm.is_moving():
-            self.steps_done += 1
-            self.pnl.center_text_at('current step: %d' % self.step_num, 3)
-            pose = {YoupiArm.MOTOR_NAMES.index(j): a for j, a in self.sequence[self.step_num].iteritems()}
-            self.arm.goto(pose, False)
+        if self.state == self.STATE_IDLE:
+            self.current_step = self.sequence[self.step_num]
+            self.state = self.STATE_LOADED
 
-            if self.steps_count == self.steps_done == 1:
-                return True
-            self.step_num = (self.step_num + 1) % len(self.sequence)
+        elif self.state == self.STATE_LOADED:
+            self.pnl.center_text_at('current step: %d' % self.step_num, 3)
+            self.logger.info('executing step #%d (%s)', self.step_num, self.current_step.KEYWORD)
+            self.current_step.execute()
+            self.state = self.STATE_EXECUTING
+
+        elif self.state == self.STATE_EXECUTING:
+            if self.current_step.is_done():
+                if self.steps_count == 1:
+                    return True
+
+                self.step_num = (self.step_num + 1) % len(self.sequence)
+                self.state = self.STATE_IDLE
+
+        else:
+            raise RuntimeError('invalid state (%s)' % self.state)
 
     def teardown(self, exit_code):
         self.pnl.center_text_at('terminating', 3)
         self.arm.soft_hi_Z()
         self.logger.info('arm set in Hi-Z')
+
+
+class SequenceStep(object):
+    def __init__(self, arm, logger, **kwargs):
+        self.arm = arm
+        self.logger = logger
+
+    def execute(self):
+        raise NotImplementedError()
+
+    def is_done(self):
+        raise NotImplementedError()
+
+
+_STATEMENTS = {}
+
+
+def register(cls):
+    try:
+        _STATEMENTS[cls.KEYWORD] = cls
+    except AttributeError:
+        pass
+    return cls
+
+
+@register
+class MoveStep(SequenceStep):
+    KEYWORD = 'MOVE'
+
+    def __init__(self, arm, logger, pose=None):
+        super(MoveStep, self).__init__(arm, logger)
+        self.pose = {YoupiArm.MOTOR_NAMES.index(j): a for j, a in pose.iteritems()}
+
+    def execute(self):
+        self.arm.goto(self.pose, False)
+
+    def is_done(self):
+        return not self.arm.is_moving()
+
+
+@register
+class PauseStep(SequenceStep):
+    KEYWORD = 'PAUSE'
+
+    def __init__(self, arm, logger, delay=1):
+        super(PauseStep, self).__init__(arm, logger)
+
+        try:
+            self.delay = int(delay)
+        except ValueError:
+            self.logger.error('invalid delay value (%s) for PAUSE', delay)
+            raise
+
+        self.limit = None
+
+    def execute(self):
+        self.limit = time.time() + self.delay
+
+    def is_done(self):
+        return time.time() >= self.limit
+
 
 
 def main():
